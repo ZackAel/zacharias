ac95a941f7d0492375f14bf9daa84f268d95c889
ZackAel <zachariaschristodoulou@hotmail.com>
2026-03-19 17:38:42 +0000

    Final local version

--- success/success.js
+++ success/success.js
@@ -1,4 +1,5 @@
 // Replace PDF_DOWNLOAD_URL in /config.js with your real hosted PDF file URL.
 document.querySelectorAll("[data-download-link]").forEach((link) => {
   link.setAttribute("href", window.APP_CONFIG.PDF_DOWNLOAD_URL);
+  link.setAttribute("download", "10-Minute-Clarity-Reset.pdf");
 });
